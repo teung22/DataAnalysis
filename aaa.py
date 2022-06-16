a0e79b6eb44dd460d6dbe6487ea3fd0f5743a1c0
teung22 <ala01950195@gmail.com>
2022-06-17 00:24:37 +0900

    first commit

---
+++ aaa.py
@@ -0,0 +1,5 @@
+name = '애플씨'
+print(name)
+print('이름=', name)
+print('이름 =', name)  #권장
+print('이름 = ', name)
---
+++ bbb.py
@@ -0,0 +1,10 @@
+#파이썬주석  
+#파이썬 대소문자 구별
+#파이썬 기본이 main함수 숨겨짐
+#파이썬 출력함수 print( '안내문' , data)
+#문제1  본인이름 출력
+
+print('white snow')
+print('winter snow')
+print('summer')
+print('-' * 100)
